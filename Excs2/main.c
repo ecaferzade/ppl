59aedfe6fcec7f446682b0303cc1919eaeebc0a0
Emirali Caferzade <46302630+ecaferzade@users.noreply.github.com>
2022-05-06 11:38:18 +0200

    Update main.c
    indentation corrected (4 spaces)

--- Excs2/main.c
+++ Excs2/main.c
@@ -4,33 +4,31 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-
-	// first initialize
-  if(spi_init()){
-    printf("ERROR: Initialization failed\n");
-    return -1;}
+    // first initialize
+    if(spi_init()){
+        printf("ERROR: Initialization failed\n");
+        return -1;
+    }
  
 
-  // do some register reading or writing, 
-  // performance commands and get status information
-  // reset CC1200
-  cc1200_cmd(SRES);
-  sleep(1);
-  writeAllRegisters();
-  cc1200_cmd(SRX);
+    // do some register reading or writing, 
+    // performance commands and get status information
+    // reset CC1200
+    cc1200_cmd(SRES);
+    sleep(1);
+    writeAllRegisters();
+    cc1200_cmd(SRX);
 	
-	while(1){
-	
-	  int packet_len = 0;
-	  int expect_packet_len = 10;
-	  char a;
+    while(1){
+        int packet_len = 0;
+	int expect_packet_len = 10;
+	char a;
 
-	  while(packet_len <= expect_packet_len){
+	while(packet_len <= expect_packet_len){
 	    cc1200_reg_read(NUM_RXBYTES, &packet_len);
-	  }
-
-
-	  for(int i = 0; i < packet_len; i++){
-	    printf("The packet is: %c", cc1200_reg_read(0x3F, &a)); 
-	  }
 	}
+
+	for(int i = 0; i < packet_len; i++){
+	    printf("The packet is: %c", cc1200_reg_read(0x3F, &a));
+	}
+    }
