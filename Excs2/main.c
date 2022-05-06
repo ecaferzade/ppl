85db226cc31dd0d88ec7c3a27b81fc85d046be77
Emirali Caferzade <46302630+ecaferzade@users.noreply.github.com>
2022-05-06 12:25:39 +0200

    Update main.c

--- Excs2/main.c
+++ Excs2/main.c
@@ -10,22 +10,24 @@ int main(int argc, char *argv[]){
         printf("ERROR: Initialization failed\n");
         return -1;
     }
- 
 
-    // do some register reading or writing, 
-    // performance commands and get status information
-    
     cc1200_cmd(SRES);  // reset CC1200
     sleep(1);
-    writeAllRegisters();
+    writeAllRegisters();  //write the register according to the desired mode
     cc1200_cmd(SRX);
+    // get status information
+    cc1200_cmd(SNOP);  //refreshes the current status
+    printf("INFO: Status:%s\n", get_status_cc1200_str());
 	
     while(1){
         int packet_len = 0;
-	int expect_packet_len = 10;
+	int expect_packet_len = 10;  //in bytes
 	char a;
 
 	while(packet_len <= expect_packet_len){
+	    /* From the datasheet: "The NUM_RXBYTES register can be polled at a given rate to get
+information about the number of bytes in the RX FIFO."
+	    */
 	    cc1200_reg_read(NUM_RXBYTES, &packet_len);
 	}
 
